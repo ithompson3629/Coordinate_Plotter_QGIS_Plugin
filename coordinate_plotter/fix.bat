4ca54ed26af7e78ec0699a34c3a27bdb868b9ede
Isaac Thompson <142573979+ithompson3629@users.noreply.github.com>
2023-09-21 12:07:25 +0100

    add new version

--- coordinate_plotter/fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\QGIS 3.28.8\bin\o4w_env.bat"
-
-
-@echo on
-pyrcc5 -o resources.py C:\Users\ithompson\AppData\Roaming\QGIS\QGIS3\profiles\PCA_QGIS_profile\python\plugins\coordinate_plotter\resources.qrc
-
